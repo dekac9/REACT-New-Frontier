8c0f76891898e838a72c657509c3f980299e5a62
dejan <dekac9@yahoo.com>
2021-09-03 12:15:07 +0200

    uradjen

---
+++ 6/zadatak/src/Forms.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+
+class Forms extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: "", godine: null, errorMessage: "" };
+    this.promeni=this.promeni.bind(this);
+  }
+  promeni = (e) => {
+    let name = e.target.name;
+    let vrednost = e.target.value;
+    let err = "";
+
+    if (name === "age") {
+      if (vrednost !== "" && !Number(vrednost)) {
+        err = "Niste uneli ispravne podatke";
+      }
+    this.setState({godine:vrednost});  
+    }else{
+    this.setState({username:vrednost});
+    }
+    this.setState({errorMessage:err});
+    
+    
+ 
+  };
+
+  render() {
+    return (
+      <div>
+        <form>
+          <h1>
+            Dobar dan: {this.state.username}, imas {this.state.godine}
+          </h1>
+          <p>
+            Unesite username:
+            <input type="text" name="username" onChange={this.promeni} />
+          </p>
+          <p>
+            Unesite godine:
+            <input type="text" name="age" onChange={this.promeni} />
+          </p>
+          {this.state.errorMessage}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Forms;
