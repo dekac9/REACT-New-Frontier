81c9aba46b00504f910581f994911e586f1b4d28
dejan <dekac9@yahoo.com>
2021-09-03 10:27:45 +0200

    u toku rada

---
+++ 5/zadatak/src/users/Users.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import User from './users/User';
+
+
+class Users extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        { ime: "Pera", godine: 30 },
+        { ime: "Mica", godine: 35 },
+        { ime: "Milos", godine: 37 },
+      ],
+      naslov: "Lista korisnika",
+    };
+    this.smanjiGodine=this.smanjiGodine.bind(this);
+  }
+
+  smanjiGodine = () => {
+    const novoStanje = this.state.users.map((element) => {
+      element.godine = element.godine - 1;
+      return element;
+    });
+    this.setState({ novoStanje });
+  };
+  render() {
+    return (
+      <div>
+        <button onClick={this.smanjiGodine}>Smanji godine</button>
+        <h1>{this.state.naslov}</h1>
+        {this.state.users.map((element) => {
+          return <User godine={element.godine}>{element.ime}</User>;
+        })}
+      </div>
+    );
+  }
+}
+
+export default Users;
