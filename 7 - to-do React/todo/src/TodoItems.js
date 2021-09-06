1182e7f682d483f7232d2f0ce93049671d48b8e8
dejan <dekac9@yahoo.com>
2021-09-06 12:19:07 +0200

    nastavak sa animacijama

--- 7 - to-do React/todo/src/TodoItems.js
+++ 7 - to-do React/todo/src/TodoItems.js
@@ -22,9 +22,13 @@ class TodoItems extends Component {
       from: { transform: 'scale(0.5, 1)' },
       to:   { transform: 'scale(1, 1)' }
     };
+    const customLeaveAnimation = {
+      from: { transform: 'scale(1, 1)' },
+      to:   { transform: 'scale(0.5, 1) translateY(-20px)' }
+    };
     return (
       <ul className="theList">
-        <FlipMove enterAnimation={customEnterAnimation}>
+        <FlipMove enterAnimation={customEnterAnimation} leaveAnimation={customLeaveAnimation}>
         {listItems}
         </FlipMove>
       </ul>
