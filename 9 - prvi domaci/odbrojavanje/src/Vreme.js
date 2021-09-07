5e3f1deb0c2ae4d1a1f23f4b58eb2b635235b0c6
dejan <dekac9@yahoo.com>
2021-09-08 00:42:05 +0200

    radi brojac trenutnog vremena

--- 9 - prvi domaci/odbrojavanje/src/Vreme.js
+++ 9 - prvi domaci/odbrojavanje/src/Vreme.js
@@ -5,18 +5,28 @@ class Vreme extends Component {
   constructor(props) {
     super(props)
     this.state={
-      datum:'trenascscutno'
+      datum:''
     };
-
+this.osvezanje=this.osvezanje.bind(this);
   }
- 
+
   
   componentDidMount(){
-    let trenutno = Date.now();
-    console.log(trenutno)
-    this.setState({datum:trenutno})
+    let trenutno = new Date();
+    const jsonDate = trenutno.toString()
+    this.setState({datum:jsonDate})
+    setInterval(() => {
+      this.osvezanje()
+    }, 1000);
 
+    
   }
+  osvezanje=()=>{
+    let trenutno = new Date();
+    const jsonDate = trenutno.toString()
+    this.setState({datum:jsonDate})
+  }
+
   
   render() { 
 
