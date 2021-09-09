1411c848c8431dea72dcb7c0dc8d5afd0095744f
dejan <dekac9@yahoo.com>
2021-09-09 09:46:21 +0200

    nastavak rada fetch api-a u React-u

--- 9 - prvi domaci/odbrojavanje/src/Vreme.js
+++ 9 - prvi domaci/odbrojavanje/src/Vreme.js
@@ -1,45 +1,63 @@
-import React, { Component } from 'react';
-import Danas from './Danas';
-import Olimpijada from './Olimpijada';
+import React, { Component } from "react";
+import Danas from "./Danas";
+import Olimpijada from "./Olimpijada";
+import Api from "./Api";
 
 class Vreme extends Component {
   constructor(props) {
-    super(props)
-    this.state={
-      trenutno_datum:'',
-      trenutno_milisekunde:'',
+    super(props);
+    this.state = {
+      trenutno_datum: "",
+      trenutno_milisekunde: "",
+      grad:''
     };
-this.osvezanje=this.osvezanje.bind(this);
+    this.osvezanje = this.osvezanje.bind(this);
+    this.onSubmit=this.onSubmit.bind(this)
   }
 
-  
-  componentDidMount(){
+  componentDidMount() {
     let trenutno_datum = new Date();
     let trenutno_vreme = Date.now();
-    const jsonDate = trenutno_datum.toString()
-    this.setState({trenutno_datum:jsonDate,trenutno_milisekunde:trenutno_vreme})
+    const jsonDate = trenutno_datum.toString();
+    this.setState({
+      trenutno_datum: jsonDate,
+      trenutno_milisekunde: trenutno_vreme,
+    });
     setInterval(() => {
-      this.osvezanje()
+      this.osvezanje();
     }, 1000);
-
-    
   }
-  osvezanje=()=>{
+  osvezanje = () => {
     let trenutno = new Date();
     let trenutno_vreme = Date.now();
-    const jsonDate = trenutno.toString()
-    this.setState({trenutno_datum:jsonDate,trenutno_milisekunde:trenutno_vreme})
-  
+    const jsonDate = trenutno.toString();
+    this.setState({
+      trenutno_datum: jsonDate,
+      trenutno_milisekunde: trenutno_vreme,
+    });
+  };
+
+onSubmit=()=>{
+  let ime=document.getElementById('grad').value;
+  this.setState({grad:ime})
+  //console.log(ime)
+}
+
+  render() {
+    return (
+      <div>
+        <Danas sada_datum={this.state.trenutno_datum}></Danas>
+        <Olimpijada sada_vreme={this.state.trenutno_milisekunde}></Olimpijada>
+        <div>
+          <input id="grad"
+            type="text"
+            placeholder="Unesite ime grada"
+          ></input>
+          <button type="submit" onClick={this.onSubmit}>Add</button>
+        </div>
+        <Api grad={this.state.grad}></Api>
+      </div>
+    );
   }
-
-  
-  render() { 
-
-    return (<div>
-      <Danas sada_datum={this.state.trenutno_datum}></Danas>
-      <Olimpijada sada_vreme={this.state.trenutno_milisekunde}></Olimpijada>
-    </div>);
-  }
-
-} 
+}
 export default Vreme;
