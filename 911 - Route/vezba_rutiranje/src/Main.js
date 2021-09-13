7e438130c0e560bdf9ddb93faf7a8d2fe405b438
dejan <dekac9@yahoo.com>
2021-09-13 16:09:12 +0200

    zavrsen zadatak rutiranje

--- 911 - Route/vezba_rutiranje/src/Main.js
+++ 911 - Route/vezba_rutiranje/src/Main.js
@@ -1,34 +1,33 @@
 import React, { Component } from "react";
-import {HashRouter,NavLink,Route} from "react-router-dom";
+import { HashRouter, NavLink, Route } from "react-router-dom";
 import Contact from "./Contact";
 import About from "./About";
 import Home from "./Home";
 
 class Main extends Component {
-
   render() {
     return (
       <HashRouter>
-      <div>
-        <h1>SinglePageAplication</h1>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li> 
-            <NavLink to="about">About</NavLink>
-          </li>
-          <li>
-            <NavLink to="contact">Contact</NavLink>
-          </li>
-        </ul>
-        <div className="sadrzaj">
-<Route path="/" component = {Home}/>
-<Route path="about" component = {About}/>
-<Route path="contact" component = {Contact}/>
+        <div>
+          <h1>SinglePageAplication</h1>
+          <ul className="header">
+            <li>
+              <NavLink exact to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="contact">Contact</NavLink>
+            </li>
+          </ul>
+          <div className="sadrzaj">
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+          </div>
         </div>
-      </div>
-</HashRouter>
+      </HashRouter>
     );
   }
 }
