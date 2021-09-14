dd5d6a4869ce5b35235676b04208e8a05ecde9c8
dejan <dekac9@yahoo.com>
2021-09-14 14:06:06 +0200

    u toku rada

--- 912 - Redux React/vezba/src/index.js
+++ 912 - Redux React/vezba/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import { Provider } from 'react-redux';
+import reducer from "./Reducer"
+import App from "./App"
+
+var store = createStore(reducer)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+  <App />
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ Reducer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function reducer(state,action){
+if(state==undefined){
+  return{count:0};
+}
+let count=state.count;
+if(action.type==="decrease"){
+  return {count:count-1}
+}else if(action.type==="increase"){
+  return {count:count+1}
+}else{
+  return state
+}
+}
+
+export default reducer
