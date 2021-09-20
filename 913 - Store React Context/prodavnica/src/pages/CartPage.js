d42d6ea16cedf085b44e364682daaaf2ed974353
dejan <dekac9@yahoo.com>
2021-09-20 16:48:07 +0200

    uveo styled components, JAKO KORISNO

--- 913 - Store React Context/prodavnica/src/pages/CartPage.js
+++ 913 - Store React Context/prodavnica/src/pages/CartPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Navigation from "../components/Navigation";
 import ShopContext from "../context/ShopContext";
+import styled from "styled-components";
 
 class CartPage extends Component {
   render() {
@@ -8,18 +9,27 @@ class CartPage extends Component {
       <ShopContext.Consumer>
         {(context) => (
           <React.Fragment>
-            <Navigation cartItemsNumber={context.cart.reduce((ukupno,elem)=>elem.quantity+ukupno),0}></Navigation>
+            <Navigation
+              cartItemsNumber={context.cart.reduce((ukupno, elem) => {
+                return elem.quantity + ukupno;
+              }, 0)}
+            ></Navigation>
+          
             <main className="products">
-            <ul>
+              <ul>
                 {context.cart.map((product) => (
                   <li key={product.id}>
                     <div>
-                      <img src={product.img} alt={product.title}/>
-                      
-                      <strong>{product.title}</strong><strong>[{product.quantity} KOMADA]</strong><span className="cena"> Cena {product.total}$</span>
+                      <img src={product.img} alt={product.title} />
+
+                      <strong>{product.title}</strong>
+                      <strong>[{product.quantity} KOMADA]</strong>
+                      <span className="cena"> Cena {product.total}$</span>
                     </div>
                     <div>
-                      <button onClick={()=>context.izbaci(product)}>Izbaci iz korpe</button>
+                      <Button onClick={() => context.izbaci(product)}>
+                        Izbaci iz korpe
+                      </Button>
                     </div>
                   </li>
                 ))}
@@ -33,3 +43,13 @@ class CartPage extends Component {
 }
 
 export default CartPage;
+
+const Button = styled.button`
+display: inline-block;
+border-radius: 3px;
+padding: 0.5rem 0;
+margin: 0.5rem 1rem;
+width: 11rem;
+background: transparent;
+color: white;
+border: 2px solid white;`
